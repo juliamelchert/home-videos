00ac344266a7004a8333460c3f3309c47cbddea7
melcherj22 <melcherj@oregonstate.edu>
2024-02-22 23:54:19 -0700

    Attempting to fix favicon

--- public/html/components/header.php
+++ public/html/components/header.php
@@ -1,7 +1,7 @@
 <html>
 
     <title>Home Videos</title>
-    <link rel="icon" type="image/x-icon" href="../../static/favicon.ico">
+    <link rel="shortcut icon" type="image/png" href="../../static/favicon.ico">
     <link rel="stylesheet" href="/static/styles.css">
 
     <head>
