ee3ee5ea2f007cf6d9938c0af20537e163e3a12f
melcherj22 <melcherj@oregonstate.edu>
2024-02-22 23:35:41 -0700

    Updated file structure to attempt to fix path errors

--- public/html/components/header.php
+++ public/html/components/header.php
@@ -2,7 +2,7 @@
 
     <title>Home Videos</title>
     <link rel="icon" type="image/x-icon" href="/static/favicon.ico">
-    <link rel="stylesheet" href="/styles.css">
+    <link rel="stylesheet" href="/static/styles.css">
 
     <head>
         <?php require_once("nav.php"); ?>
