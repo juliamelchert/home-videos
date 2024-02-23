62fc6edcf7df906a5f4f5d5119455ac8a3f6df87
melcherj22 <melcherj@oregonstate.edu>
2024-02-22 23:29:21 -0700

    Attempting to fix 404 for CSS file path

--- html/components/header.php
+++ html/components/header.php
@@ -1,11 +1,10 @@
 <html>
 
     <title>Home Videos</title>
-    <link rel="icon" type="image/x-icon" href="/Images/favicon.ico">
-    <link rel="stylesheet" href="/CSS/styles.css">
+    <link rel="icon" type="image/x-icon" href="/static/favicon.ico">
+    <link rel="stylesheet" href="/home-videos/static/styles.css">
 
     <head>
-        <base href="https://melchert-home-videos-27d7619e9db7.herokuapp.com/">
         <?php require_once("nav.php"); ?>
     </head>
 
