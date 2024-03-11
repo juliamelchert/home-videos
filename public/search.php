90a25ebe8b6fbd1c50381908bf45b34e7a728298
melcherj22 <melcherj@oregonstate.edu>
2024-03-11 11:34:37 -0600

    Updated input/search button to become a form

--- public/search.php
+++ public/search.php
@@ -1,12 +1,14 @@
-<?php require_once("./html/components/header.php"); ?>
+<?php require_once("./components/header.php"); ?>
 
     <div class="page-title">
 		<h1>Search</h1>
 	</div>
 
-    <div class="search-area">
-        <input id="search-bar" type="text" placeholder="Search for videos by title, date, tags...">
-        <img id="search-icon" src="../../static/search-icon.png">
-    </div>
+    <form method="get" action="./handlers/search_handler.php">
+        <div class="search-area">
+            <input id="search-bar" type="text" name="search" placeholder="Search for videos by title, date, tags...">
+            <button type="submit" id="btn-search"><img id="search-icon" src="./static/search-icon.png"></button>
+        </div>
+    </form>
 
-<?php require_once("./html/components/footer.php"); ?>
+<?php require_once("./components/footer.php"); ?>
