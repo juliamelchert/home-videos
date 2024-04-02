2b3837dc6728353d7ed5640d942eb47cb238fe40
melcherj22 <melcherj@oregonstate.edu>
2024-04-01 23:12:07 -0600

    Refactored for loop

--- public/handlers/search_handler.php
+++ public/handlers/search_handler.php
@@ -16,12 +16,11 @@
 
     // Remove any duplicates
     $videoTitles = [];
-    $i = 0;
-    foreach ($allVideos as $video) {
-        if (in_array($video['title'], $videoTitles)) {
+    for ($i = 0; $i < count($allVideos); $i++) {
+        if (in_array($allVideos[$i]['title'], $videoTitles)) {
             unset($allVideos[$i]);
         } else {
-            $videoTitles[] = $video['title'];
+            $videoTitles[] = $allVideos[$i]['title'];
         }
         $i++;
     }
