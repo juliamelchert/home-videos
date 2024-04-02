f221ad8570e7670bb3cfaf94f324d9a134244505
melcherj22 <melcherj@oregonstate.edu>
2024-04-01 21:52:42 -0600

    Changed delete button to make it more obviously a delete button

--- public/browse.php
+++ public/browse.php
@@ -14,7 +14,7 @@
 						<div class='video-header'>
 							<span>" . htmlspecialchars($title) . "</span>";
 				if (isset($_SESSION['admin']) and $_SESSION['admin'] == 1) {
-					echo "<a href='./handlers/delete_video.php?title=" . htmlspecialchars($title) . "'><button id='btn-delete'>X</button></a>";
+					echo "<a href='./handlers/delete_video.php?title=" . htmlspecialchars($title) . "' title='Delete'><button class='btn-delete'>X</button></a>";
 				}
 				echo "</div>";
 				echo "<a href=" . htmlspecialchars($url) . "><img class='thumbnail' src='http://img.youtube.com/vi/" . htmlspecialchars($video_id) . "/hqdefault.jpg' title='" . htmlspecialchars($title) . "' alt='YouTube Thumbnail' /></a></div>";
