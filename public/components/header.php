a9d382751b426be4e39d4f38afd4b52001cd1032
melcherj22 <melcherj@oregonstate.edu>
2024-04-17 14:54:57 -0600

    Attempting to fix heroku custom font issue

--- public/components/header.php
+++ public/components/header.php
@@ -6,9 +6,12 @@
 
     <head>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Platypi" rel="stylesheet" type="text/css">
+        <!--
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,100..900;1,100..900&family=Platypi:ital,wght@0,300..800;1,300..800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">            
+        <link href="https://fonts.googleapis.com/css2?family=Platypi:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+-->
         <?php
             require_once("nav.php");
             if (!isset($_SESSION['authenticated']) or !$_SESSION['authenticated']) {
