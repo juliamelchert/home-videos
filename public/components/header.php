4e5725be3e371ee935707c805f6c820952ec3361
melcherj22 <melcherj@oregonstate.edu>
2024-04-17 14:58:49 -0600

    Attempting to fix heroku custom font issue

--- public/components/header.php
+++ public/components/header.php
@@ -7,7 +7,7 @@
     <head>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
         <style>
-            @import url('https://fonts.googleapis.com/css2?family=Platypi:ital,wght@0,300..800;1,300..800&display=swap');
+            @import url('//fonts.googleapis.com/css2?family=Platypi:ital,wght@0,300..800;1,300..800&display=swap');
         </style>
         <?php
             require_once("nav.php");
